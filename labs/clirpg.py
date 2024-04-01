312025c31abea8c5d3a096ceaa2e59bcefaa6d88
jake-bestland <beja0405@gmail.com>
2024-04-01 13:33:05 -0500

    added name length check for API call

--- labs/clirpg.py
+++ labs/clirpg.py
@@ -63,8 +63,11 @@ url = f"https://uzby.com/api.php?min={min_name_len}&max={max_name_len}"
 drag_name = requests.get(url).text
 ev_kngt_name = requests.get(url).text
 
-name = input("What is your name?: ")
-user_name_len = len(name)
+user_name_len = 0
+while not (user_name_len >= 2 and user_name_len <= 40):
+    name = input("What is your name?: ")
+    user_name_len = len(name)
+    
 un_url = f"https://uzby.com/api.php?min={user_name_len}&max={user_name_len}"
 user_game_name = requests.get(un_url).text
 
